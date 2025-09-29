d5f20a55021647084104062fb59c4cf211c70215
Vaalyn <vaalyndev@gmail.com>
2025-09-29 18:19:12 +0200

    Add shell scripts

---
+++ normalize-radio.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker compose -f docker-compose.radio.yml up
+docker compose -f docker-compose.radio.yml down
---
+++ normalize-tv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker compose -f docker-compose.tv.yml up
+docker compose -f docker-compose.tv.yml down
